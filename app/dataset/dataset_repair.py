96fe0a4e921eab41b761f435328ecde5e7172fff
Bhavanasi Devendranath <83875341+Devroyal1@users.noreply.github.com>
2025-08-30 19:26:21 +0530

    Add files via upload

--- app/dataset/dataset_repair.py
+++ app/dataset/dataset_repair.py
@@ -1,1 +1,21 @@
+import csv
 
+# Specify your input and output file names
+input_file = './orignal_cardio_train.csv'  # Input file with semicolon delimiters
+output_file = './cardiovascular_clean_dataset.csv'  # Output file with comma delimiters
+
+# Open the input file for reading
+with open(input_file, mode='r', newline='') as infile:
+    # Create a CSV reader object with semicolon as delimiter
+    reader = csv.reader(infile, delimiter=';')
+
+    # Open the output file for writing
+    with open(output_file, mode='w', newline='') as outfile:
+        # Create a CSV writer object with comma as delimiter
+        writer = csv.writer(outfile, delimiter=',')
+
+        # Write each row from the reader to the writer
+        for row in reader:
+            writer.writerow(row)
+
+print("Delimiter successfully changed from ';' to ','")
